a9182f107abe2c30db3e7ee7bbef74fca7e33049
Slickroot <elaich@gmx.net>
2020-08-31 23:26:45 +0200

    qutebrowser font size, downloads bar remove when finished

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,3 +2,7 @@ config.set('content.notifications', False, 'https://www.reddit.com')
 
 c.statusbar.show = 'in-mode'
 c.tabs.show = 'switching'
+c.fonts.default_size = '8pt'
+
+# remove
+c.downloads.remove_finished = 5000
