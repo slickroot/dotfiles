a122168a1de9740d0787f9f6be4f0dbba72957f4
Marouane Elaich <elaich@gmx.net>
2024-08-17 06:32:45 +0100

    shora_start handles backend also

--- bin/.local/bin/shora_start.sh
+++ bin/.local/bin/shora_start.sh
@@ -1,5 +1,12 @@
-kitty @ launch --type=tab --cwd $HOME/work/shora/shora-web --tab-title "Web" --keep-focus
-kitty @ launch --type=tab --cwd $HOME/work/shora/shora-web --tab-title "Vim" --hold --keep-focus nvim 
-kitty @ launch --type=tab --cwd $HOME/work/shora/shora-web --tab-title "Git" --hold --keep-focus git status
-kitty @ launch --type=tab --cwd $HOME/work/shora/shora-web --tab-title "Shell" --keep-focus
-kitty @ close-tab --match "state:focused"
+WEB_HOME=$HOME/work/shora/shora-web
+API_HOME=$HOME/work/shora/shora-api
+
+kitty @ launch --type=os-window --cwd $WEB_HOME --tab-title "Web" --hold npm run dev
+kitty @ launch --type=tab --cwd $WEB_HOME --tab-title "Vim" --hold --keep-focus nvim 
+kitty @ launch --type=tab --cwd $WEB_HOME --tab-title "Git" --hold --keep-focus git status
+kitty @ launch --type=tab --cwd $WEB_HOME --tab-title "Shell" --keep-focus
+
+kitty @ launch --type=os-window --cwd $API_HOME --title "API" --hold npm run start:dev
+kitty @ launch --type=tab --cwd $API_HOME --tab-title "Vim" --hold --keep-focus nvim 
+kitty @ launch --type=tab --cwd $API_HOME --tab-title "Git" --hold --keep-focus git status
+kitty @ launch --type=tab --cwd $API_HOME --tab-title "Shell" --keep-focus
